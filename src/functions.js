3a15753e4e6ffcd0dc97d9ecb004b1f70d41a800
cyb2void@gmail.com <cyb2void@gmail.com>
2024-11-05 11:25:12 +0000

    Edited files:     /src/functions.js

--- src/functions.js
+++ src/functions.js
@@ -1,16 +1,20 @@
 var usedPairs = [];
+
 function getRandomPair(pairs) {
-    if (pairs.length === 0) {
+    if (Array.isArray(pairs) && pairs.length > 0) {
+        var filteredPairs = pairs.filter(function(pair) {
+            return !usedPairs.includes(pair);
+        });
+
+        if (filteredPairs.length > 0) {
+            var randomIndex = Math.floor(Math.random() * filteredPairs.length);
+            var pair = filteredPairs[randomIndex];
+            usedPairs.push(pair);
+            return pair;
+        } else {
+            return null;
+        }
+    } else {
         return null;
     }
-    var filteredPairs = pairs.filter(function(pair) {
-         return !usedPairs.includes(pair);
-    });
-    if (filteredPairs.length === 0) {
-        return null;
-    }
-    var randomIndex = Math.floor(Math.random() * filteredPairs.length);
-    var pair = filteredPairs[randomIndex];
-    usedPairs.push(pair);
-    return pair;
 }
