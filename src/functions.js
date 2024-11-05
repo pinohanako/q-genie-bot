3c04fd8dd7a97e6ceb2f57f384934cbc0ba061b9
cyb2void@gmail.com <cyb2void@gmail.com>
2024-11-05 12:51:41 +0000

    Edited files:     /src/functions.js

--- src/functions.js
+++ src/functions.js
@@ -3,7 +3,7 @@ function getRandomPair(pairs) {
     if (pairs.length === 0) {
         return null;
     }
-    pairs = pairs.filter(pair => !usedPairs.includes(pair));
+    pairs = pairs.filter(pair = !usedPairs.includes(pair));
     var randomIndex = Math.floor(Math.random() * pairs.length);
     var pair = pairs[randomIndex];
     usedPairs.push(pair);
